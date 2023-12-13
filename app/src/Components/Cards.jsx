1f004aeeadbbbb7d83aa7999144aebb8d75f2eb4
Saksham Shrivastava <146808515+sakshamshrivastava28111810@users.noreply.github.com>
2023-12-13 15:32:27 +0530

    13/12/2023 | 03:32 PM
    different approaches of this logic

--- app/src/Components/Cards.jsx
+++ app/src/Components/Cards.jsx
@@ -1,28 +1,36 @@
 import React, { Fragment } from 'react';
 import Card from './Card';
 
-const Cards = ({courses}) => {
+const Cards = ({ courses }) => {
   console.log(courses);
 
   let allCourses = [];
-
-  const getCourses = () =>{
-    Object.values(courses).forEach((coursesCategory)=>{
-      coursesCategory.forEach((course)=>{
-          allCourses.push(course);
+//approach 1
+  const getCourses = () => {
+    Object.values(courses).forEach((coursesCategory) => {
+      coursesCategory.forEach((course) => {
+        allCourses.push(course);
       })
 
     })
     return allCourses;
   }
+//approach 2
+  // const getCourses = () => {
+  //   return [].concat(...Object.values(courses));
+  // }; 
+//approach 3
+  // const getCourses = () => {
+  //   return Object.values(courses).flatMap(courseCategory => courseCategory);
+  // };
   return (
-<Fragment>
-  {
-    getCourses().map((course)=>{
-      <Card/>
-    })
-  }
-</Fragment>
+    <Fragment>
+      {
+        getCourses().map((course) => {
+          <Card />
+        })
+      }
+    </Fragment>
   )
 }
 
